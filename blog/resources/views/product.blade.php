4c2939df34aab16dbde639c1a126cf02ef4db056
nmsdzbtmt <nmsdzbmt@gmail.com>
2020-12-21 19:36:40 +0700

    create index sanpham

---
+++ blog/resources/views/product.blade.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quản lý bán hàng</title>
+  <link rel="stylesheet" href="css/all.css">
+<body>
+  <div class="sidebar-container">
+    <div class="sidebar-logo">
+        <a href="index.html">MILKTEA TSU</a>
+    </div>
+    <ul class="sidebar-navigation">
+      <li class="header">QUẢN LÝ</li>
+      <li>
+        <a href="sanpham.html">
+          <i class="fa fa-home" aria-hidden="true"></i>SẢN PHẨM
+        </a>
+      </li>
+      <li>
+        <a href="nhanvien.html">
+          <i class="fa fa-tachometer" aria-hidden="true"></i>NHÂN VIÊN
+        </a>
+      </li>
+      <li class="header">HỆ THỐNG</li>
+      <li>
+        <a href="lichsuhoado.html">
+          <i class="fa fa-users" aria-hidden="true"></i>HÓA ĐƠN
+        </a>
+      </li>
+      <li>
+        <a href="doanhthu.html">
+          <i class="fa fa-cog" aria-hidden="true"></i>DOANH THU
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="content-container">
+
+    <div class="container-fluid">
+      <div class="jumbotron">
+        <h1 style="text-align: center">DANH SÁCH SẢN PHẨM CỦA MILKTEA UST</h1>
+        <form action="">
+            <input type="text" placeholder="Search..">
+            <input type="button" value="Search">
+        </form>
+        <input type="button" value="ADD" style="margin-left: 91%; width: 60px;margin-top: 20px;">
+        <table border="1px" align="center" style="width:80%;margin-top: 20px;">
+            <tr>
+              <th style="width: 60px;">STT</th>
+              <th>Tên Sản Phẩm</th>
+              <th>Giá Bán</th>
+              <th style="width: 70px;">Xóa</th>
+              <th style="width: 70px;">Sửa</th>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">1</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">2</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">3</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">4</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">5</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">6</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">7</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">8</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">9</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr><tr>
+              <td style="width: 60px; text-align: center;">10</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">11</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">12</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">13</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">14</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">15</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">16</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+            <tr>
+              <td style="width: 60px; text-align: center;">17</td>
+              <td style="text-align: center;">Trà sữa chân trâu</td>
+              <td style="text-align: center;">20.000đ</td>
+              <td><input type="button" value="xóa" style="width:100%"></td>
+              <td><input type="button" value="sửa" style="width:100%"></td>
+            </tr>
+
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
